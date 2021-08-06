33b331b2f8c2b5211b4a31228af917b86060b6f6
Francesca Bitto <54183981+Francesca-Bit@users.noreply.github.com>
2021-08-06 17:07:54 +0100

    add properties type

---
+++ propertiestype.js
@@ -0,0 +1,38 @@
+export const PROPERTY_STRING_BY_RANGE={'string':'xsd:string',
+										'json':'xdd:json',
+										'base64Binary':'xsd:base64Binary',
+										'html':'xdd:html',
+										'url':'xdd:url',
+										'email':'xdd:email'};
+
+export const PROPERTY_NUMBER_BY_RANGE={ 'integer':'xsd:integer',
+										'decimal':'xsd:decimal',
+										'long':'xsd:long',
+										'byte':'xsd:byte',
+										'short':'xsd:short',
+										'decimalRange':'xdd:decimalRange',
+										'unsignedByte':'xsd:unsignedByte',
+										'unsignedInt':'xsd:unsignedInt',
+										'unsignedLong':'xsd:unsignedLong',
+										'positiveInteger':'xsd:positiveInteger',
+										'nonNegativeInteger':'xsd:nonNegativeInteger',
+										'nonPositiveInteger':'xsd:nonPositiveInteger',
+										'negativeInteger':'xsd:negativeInteger'}
+
+
+export const PROPERTY_GEO_BY_RANGE={'coordinate':'xdd:coordinate',
+									'coordinatePolyline':'xdd:coordinatePolyline',
+									'coordinatePolygon':'xdd:coordinatePolygon'}
+
+export const PROPERTY_TEMPORAL_BY_RANGE={'dateTime':'xsd:dateTime',
+										 'gYear':'xsd:gYear',
+										 'gYearRange':'xdd:gYearRange',
+										 'gMonth':'xsd:gMonth',
+										 'gDay':'xsd:gDay',
+										 'gYearMonth':'xsd:gYearMonth',
+										 'gMonthDay':'xsd:gMonthDay',
+										 'duration':'xsd:duration',
+										 'dateRange':'xdd:dateRange',
+										 'date':'xsd:date',
+										 'time':'xsd:time',
+										 'dateTimeStamp':'xsd:dateTimeStamp'}
